fe382c8e0ebc76161e6dc75889eb2109871e400d
deadmau6 <joepop16@gmail.com>
2016-10-25 14:22:14 -0600

    did little

--- simulation.py
+++ simulation.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     
     #add all the links
     link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link.Link(router_a, 0, server, 0, 50))
+    link_layer.add_link(link.Link(router_a, 0, server, 0, 30))
     
     
     #start all the objects
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     
     #create some send events    
     for i in range(3):
-        client.udt_send(2, 'Sample data %d' % i)
+        client.udt_send(2, 'AfterrefactoringmycodeabitIhavebeenabletoachievetheobjectiveIsetouttoaccomplish.')
     
     
     #give the network sufficient time to transfer all packets before quitting
