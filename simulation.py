1991ee8f75d0d4364ecb1f32205ba915a3e68fcd
epennell <e-penn@hotmail.com>
2016-10-25 19:09:55 -0600

    Started on prob 3.

--- simulation.py
+++ simulation.py
@@ -16,29 +16,46 @@ if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
     table = [0,0,0,0,0,0,0,0]
     #create network nodes
-    client = network.Host(1, 50)
+    host1 = network.Host(1, 50)
     table.insert(1, 50)
-    object_L.append(client)
-    server = network.Host(2, 30)
-    table.insert(2, 30)
-    object_L.append(server)
+    object_L.append(host1)
+    host2 = network.Host(2, 50)
+    table.insert(2, 50)
+    object_L.append(host2)
+    host3 = network.Host(3, 50)
+    table.insert(3, 50)
+    object_L.append(host3)
     router_a = network.Router(*table, name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
-    
+    router_b = network.Router(*table, name='B', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_b)
+    router_c = network.Router(*table, name='C', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_c)
+    router_d = network.Router(*table, name='D', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_d)
+
     #create a Link Layer to keep track of links between network nodes
     link_layer = link.LinkLayer()
     object_L.append(link_layer)
     
     #add all the links
-    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link.Link(router_a, 0, server, 0, 30))
-    
+    link_layer.add_link(link.Link(host1, 0, router_a, 2, 50))
+    link_layer.add_link(link.Link(host2, 1, router_a, 2, 50))
+    link_layer.add_link(link.Link(router_a, 2, router_b, 3, 50))
+    link_layer.add_link(link.Link(router_a, 2, router_c, 4, 50))
+    link_layer.add_link(link.Link(router_b, 3, router_d, 5, 50))
+    link_layer.add_link(link.Link(router_c, 4, router_d, 5, 50))
+    link_layer.add_link(link.Link(router_d, 5, host3, 6, 50))
     
     #start all the objects
     thread_L = []
-    thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
-    thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=host1.__str__(), target=host1.run))
+    thread_L.append(threading.Thread(name=host2.__str__(), target=host2.run))
+    thread_L.append(threading.Thread(name=host3.__str__(), target=host3.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
     
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
     
@@ -48,7 +65,7 @@ if __name__ == '__main__':
     
     #create some send events    
     for i in range(3):
-        client.udt_send(2, 'AfterrefactoringmycodeabitIhavebeenabletoachievetheobjectiveIsetouttoaccomplish.')
+        host1.udt_send(2, 'AfterrefactoringmycodeabitIhavebeenabletoachievetheobjectiveIsetouttoaccomplish.')
     
     
     #give the network sufficient time to transfer all packets before quitting
